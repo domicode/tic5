617d1a2ae95404fb15d4fbe7d5ab83e885296242
domicode <domi@domidesign.ch>
2014-04-25 19:10:30 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+// function mark(target) {
+//   target.innerHTML = "X"
+// }
+
+// var target = $(".line1.row1")[0]
+
+// mark(target);
+
+$(document).ready( function() {
+    // $(".box").click(function(event) {
+    //   event.currentTarget.innerHTML = "X";
+    // }); 
+
+    var turn = 1
+
+    var player1 = [];
+    var player2 = [];
+  
+    $(".box").click(function(event) {
+      if (event.currentTarget.innerHTML == "") {
+        if (turn % 2 == 0) {
+          event.currentTarget.innerHTML = "x";
+          player1.push(parseInt(event.currentTarget.id));
+          winning(player1);
+        } else {
+          event.currentTarget.innerHTML = "o";
+          player2.push(parseInt(event.currentTarget.id));
+          winning(player2);
+        }
+        turn += 1
+      }
+    });
+
+    function checksum(player) {
+      // var sum = 0;
+      // for (var i = 0; i < 3; i++) {
+      //   var sum = player[i];
+      // };
+      var sum = 0;
+      for (var i in player) {
+        sum += player[0];
+        sum += player[1];
+        sum += player[2];
+      }
+      if (sum == 15) { return true };
+    }
+
+    function winning(player) {
+      console.log(player)
+      console.log(checksum(player))
+      if (player.length >= 3) {
+        console.log(checksum(player))
+        if (checksum(player)) {
+          $("#winning").html("You won the game");
+        }
+      }
+    }
+
+
+  });
